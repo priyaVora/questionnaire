87e3c9dae13fba9d044a5c3c33f6149dab684b7c
Priya <prvora89@gmail.com>
2019-08-28 10:11:41 -0600

    Admin routes and user routes are separated.

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,106 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb+srv://dbUser:dbUserPassword@cluster0-t2kyl.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'questionnaire_db';
+
+var mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
 var router = express.Router();
+
+router.route("/viewusers").get(
+    function (req, res) {
+        try {
+            console.log("View users from admin...");
+            MongoClient.connect(url, mongoOptions, function (err, client) {
+                assert.equal(null, err);
+                const db = client.db(dbName);
+                db.collection('Users')
+                    .find()
+                    .toArray().then(users => {
+                        var model = {
+                            title: "Users!",
+                            rawr: users
+                        };
+                        res.render("viewusers", model);
+                    })
+
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+);
+
+
+router.route('/user/:username/suspend').get(
+    function (req, res) {
+        const {
+            username
+        } = req.params;
+        try {
+            MongoClient.connect(url, mongoOptions, function (err, client) {
+                assert.equal(null, err);
+                const db = client.db(dbName);
+                var myquery = {
+                    username: username
+                };
+                var newvalues = {
+                    $set: {
+                        status: "suspended"
+                    }
+                };
+
+                db.collection("Users").updateOne(myquery, newvalues, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document updated");
+                });
+                res.redirect('http://localhost:3000/admin/viewusers');
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+);
+
+router.route('/user/:username/activate').get(
+    function (req, res) {
+        const {
+            username
+        } = req.params;
+        try {
+            MongoClient.connect(url, mongoOptions, function (err, client) {
+                assert.equal(null, err);
+                const db = client.db(dbName);
+                var myquery = {
+                    username: username
+                };
+                var newvalues = {
+                    $set: {
+                        status: "active"
+                    }
+                };
+
+                db.collection("Users").updateOne(myquery, newvalues, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document updated");
+                });
+                res.redirect('http://localhost:3000/admin/viewusers');
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+);
+
+
 
 
 module.exports = router;
+console.log("Admin routes are running.");
--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,99 @@
 const express = require('express');
+const port = 3000;
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const assert = require('assert');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+
+// Mongo variables
+// Connection URL
+const url = 'mongodb+srv://dbUser:dbUserPassword@cluster0-t2kyl.gcp.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'questionnaire_db';
+
+var mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+
 var router = express.Router();
 
-router.route("/").get(function(req, res) {
+router.route("/").get(
+    function (req, res) {
+        res.render("index");
+    }
+);
+router.route('/login').get(
+    function (req, res) {
+        res.render("login");
+    }
+);
+router.route('/register').get(
+    function (req, res) {
+        res.render("register");
+    }
+);
 
-    res.render("index");
-});
+router.route('/login').post(
+    function (req, res) {
+        let pw = req.body.password;
+        (async function mongo() {
+            try {
+                var client = await MongoClient.connect(url, mongoOptions);
+                var db = client.db(dbName);
+                return db.collection('Users')
+                    .findOne({
+                        "username": req.body.username
+                    });
+            } catch (err) {
+                console.log(err);
+            } finally {
+                client.close();
+            }
+        }()).then(user => {
+            if (user.status === "active") {
+                if (bcrypt.compareSync(pw, user.hash)) {
+                    res.redirect('http://localhost:3000/');
+                }
+            } else {
+                res.render("suspended");
+            }
+        })
+    }
+);
 
-router.route("/login").get(function(req, res) {
+router.route('/register').post(
+    function (req, res) {
+        (async function mongo() {
+            try {
+                const hash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync())
+                let obj = {
+                    username: req.body.username,
+                    hash: hash,
+                    user_level: 'user',
+                    email: req.body.email,
+                    age: req.body.age,
+                    status: 'active',
+                    answer1: req.body.flavor,
+                    answer2: req.body.message,
+                    answer3: req.body.season
+                };
 
-    res.render("login");
-});
-
-router.route("/register").get(function(req, res) {
-
-    res.render("register");
-});
-
-
-
+                var client = await MongoClient.connect(url, mongoOptions);
+                var db = client.db(dbName);
+                db.collection("Users").insertOne(obj, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                });
+                res.redirect('http://localhost:3000/login');
+            } catch (err) {
+                console.log(err);
+            } finally {
+                client.close();
+            }
+        }());
+    }
+)
 module.exports = router;
